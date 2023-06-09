d414d040f29795b1f76a0435231716e5ea28279a
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2023-06-09 12:32:58 -0600

    agregue aviso

---
+++ avisodeprivacidad.php
@@ -0,0 +1,19 @@
+<?php
+include './includes/templates/header.php';
+include './includes/templates/funciones.php';
+?>
+
+<!-- Generamos el encabezado -->
+<?php
+genera_encabezado_de_pagina("assets/images/pages/bg_coloquios.jpg", "Aviso de Privacidad");
+?>
+<!-- Fin de - Generamos el encabezado -->
+
+
+
+
+
+
+<?php
+include './includes/templates/footer.php';
+?>
